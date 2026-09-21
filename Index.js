df8bad8a9c74e5f83083548a0218140bd91ec512
LeonardoRegueira <leonardoregueira1991@gmail.com>
2026-09-21 16:28:01 -0300

    funciones con o sin parametro y retorno

---
+++ Index.js
@@ -0,0 +1,30 @@
+console.clear(); // Limpieza de terminal
+
+//Recibe Parámetro - NO Retorna valor
+function saludar (nom){
+    console.log('Hola bienvenido al sito sin retorno', nom)
+}
+saludar("Leonardo")
+
+//Recibe Parámetro - Retorna valor
+function saludarConRetorno (nom){
+    return 'Hola bienvenido al sito con retorno! '+nom
+}
+let retornoSaludar = saludarConRetorno("Gabriela")
+console.log(retornoSaludar)
+
+
+
+//No recibe Parámetro - NO Retorna valor
+function saludarSinParametro (){
+    console.log('Hola bienvenido al sito sin Parámetro y valor')
+}
+saludarSinParametro()
+
+//No recibe Parámetro - Retorna valor
+function saludarSinParamRet(){
+    return "Hola bienvenido al sito sin parametro"
+}
+console.log(saludarSinParamRet())
+console.log(saludarSinParamRet)
+
